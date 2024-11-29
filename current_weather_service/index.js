c9f6139761dd263c991ece19c08f6441510d101c
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-11-29 16:07:22 -0500

    cors fix

--- current_weather_service/index.js
+++ current_weather_service/index.js
@@ -3,7 +3,6 @@ const currentWeatherService = require('./currentWeatherService');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 
 let selectedLatitude = null
