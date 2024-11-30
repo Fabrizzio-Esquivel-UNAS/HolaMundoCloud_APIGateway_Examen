00cc067359e2e2b76647d93e655353ecd3754e1d
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-11-29 20:59:55 -0500

    fix 13

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,27 @@
-const api1 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/instance1';
-const api2 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/instance1_5';
-const api3 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/instance2';
-// const api1 = 'http://localhost:443';
-// const api2 = 'http://localhost:80';
-// const api3 = 'http://localhost:3000';
+const api1 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/current_weather_service/current-weather';
+const api2 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/forecast_service';
+const api3 = 'https://8k153r2mp0.execute-api.us-east-2.amazonaws.com/stage/historical_weather_service';
+// const api1 = 'http://localhost:443/current-weather';
+// const api2 = 'http://localhost:80/forecast';
+// const api3 = 'http://localhost:3000/historical';
 
 async function getCurrentWeather() {
     const lat = document.getElementById('latInput').value;
     const long = document.getElementById('longInput').value;
-    const response = await fetch(`${api1}/current-weather?lat=${lat}&long=${long}`);
+    const response = await fetch(`${api1}?lat=${lat}&long=${long}`);
     const data = await response.json();
     displayOutput(data);
 }
 
 async function getForecast() {
-    const response = await fetch(`${api2}/forecast`);
+    const response = await fetch(`${api2}`);
     const { data, elapsed } = await response.json();
     displayOutput({ elapsedTime: `${elapsed} ms`, data });
 }
 
 async function getHistoricalWeather() {
     const date = document.getElementById('historyDate').value;
-    const response = await fetch(`${api3}/historical?date=${date}`);
+    const response = await fetch(`${api3}?date=${date}`);
     const { data, elapsed } = await response.json();
     displayOutput({ elapsedTime: `${elapsed} ms`, data });
 }
